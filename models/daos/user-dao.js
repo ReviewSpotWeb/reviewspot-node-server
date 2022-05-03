b63da4fa5b5bcabea0562a6ee8bf8de2586fde69
Eli MacColl <elimaccoll@gmail.com>
2022-05-03 13:30:09 -0400

    added marking reports as dismissed for a banned user

--- models/daos/user-dao.js
+++ models/daos/user-dao.js
@@ -1,71 +1,80 @@
 import { Comment } from "../comment.js";
 import { User } from "../user.js";
 import { Review } from "../review.js";
+import { Report } from "../report.js";
 
 const getUserById = async (userId) => {
-    try {
-        const user = await User.findById(userId);
-        return [user, null];
-    } catch (error) {
-        return [null, error];
-    }
+  try {
+    const user = await User.findById(userId);
+    return [user, null];
+  } catch (error) {
+    return [null, error];
+  }
 };
 
 const getUserStats = async (userId) => {
-    try {
-        const numComments = await Comment.count({
-            "authorInfo.authorId": userId,
-        });
-        const numReviews = await Review.count({
-            "authorInfo.authorId": userId,
-        });
+  try {
+    const numComments = await Comment.count({
+      "authorInfo.authorId": userId,
+    });
+    const numReviews = await Review.count({
+      "authorInfo.authorId": userId,
+    });
 
-        return [[numReviews, numComments], null];
-    } catch (error) {
-        return [null, error];
-    }
+    return [[numReviews, numComments], null];
+  } catch (error) {
+    return [null, error];
+  }
 };
 
 const updateUserBio = async (userId, newBio) => {
-    try {
-        const userToUpdate = await User.findByIdAndUpdate(userId, {
-            bio: newBio,
-        });
-        return [userToUpdate, null];
-    } catch (error) {
-        return [null, error];
-    }
+  try {
+    const userToUpdate = await User.findByIdAndUpdate(userId, {
+      bio: newBio,
+    });
+    return [userToUpdate, null];
+  } catch (error) {
+    return [null, error];
+  }
 };
 
 // TODO: This should ABSOLUTELY be a transaction.
 const banUser = async (userId) => {
-    try {
-        const userToBan = await User.findByIdAndUpdate(userId, {
-            banned: true,
-        });
-        const reviewsDeleted = await Review.deleteMany({
-            "authorInfo.authorId": userId,
-        });
-        const commentsDeleted = await Comment.deleteMany({
-            "authorInfo.authorId": userId,
-        });
-        const likedReviews = await Review.updateMany(
-            { likedBy: userId },
-            {
-                $pull: {
-                    likedBy: userId,
-                },
-            }
-        );
-        return [userToBan, null];
-    } catch (error) {
-        return [null, error];
-    }
+  try {
+    const userToBan = await User.findByIdAndUpdate(userId, {
+      banned: true,
+    });
+    const reviewsDeleted = await Review.deleteMany({
+      "authorInfo.authorId": userId,
+    });
+    const commentsDeleted = await Comment.deleteMany({
+      "authorInfo.authorId": userId,
+    });
+    const likedReviews = await Review.updateMany(
+      { likedBy: userId },
+      {
+        $pull: {
+          likedBy: userId,
+        },
+      }
+    );
+    const reportsDeleted = await Report.updateMany(
+      { authorId: userId },
+      {
+        $set: {
+          dismissed: true,
+        },
+      }
+    );
+    return [userToBan, null];
+  } catch (error) {
+    return [null, error];
+  }
 };
 
 export default {
-    getUserById,
-    getUserStats,
-    updateUserBio,
-    banUser,
+  getUserById,
+  getUserStats,
+  updateUserBio,
+  banUser,
 };
